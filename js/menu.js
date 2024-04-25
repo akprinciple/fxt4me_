cb60299f14989424139c65b42cfac90baaf7a39d
akprinciple <akeemolayiwola9@gmail.com>
2024-04-25 10:07:34 +0100

    Fxt4me first commit

---
+++ js/menu.js
@@ -0,0 +1,76 @@
+header.innerHTML = `<div class="container">
+<nav class="navbar navbar-expand-lg navbar-dark p-0">
+    <a href="index.html" class="navbar-brand">
+        
+       
+        <img src='img/fxt4me logo.png' style='width: 150px; background: white; padding: 10px'>
+    </a>
+    <button type="button" class="navbar-toggler ms-auto me-0" data-bs-toggle="collapse"
+        data-bs-target="#navbarCollapse">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarCollapse">
+        <div class="navbar-nav ms-auto">
+            <a href="index" class="nav-item nav-link active">Home</a>
+            <a href="about" class="nav-item nav-link">About Us</a>
+            <a href="pricing" class="nav-item nav-link">Plans</a>
+            <a href="faq" class="nav-item nav-link">Faq</a>
+            <a href="contact" class="nav-item nav-link">Contact</a>
+            <a href="register" class="nav-item nav-link border border-light text-white mb-1 px-2 py-1 rounded">Sign Up</a>
+            <a href="login" class="nav-item nav-link bg-light text-primary px-2 py-1 rounded">Login</a>
+            
+        </div>
+        
+    </div>
+</nav>
+</div>`
+
+footer.innerHTML = `<div class="container py-5">
+            <div class="row g-5">
+                <div class="col-md-6 col-lg-6 wow fadeIn" data-wow-delay="0.1s">
+                    <a href="index" class="d-inline-block mb-3">
+                        <img src="img/fxt4me logo.png" alt="" width="150px" style='padding: 10px; background: white;'>
+                    </a>
+                    <p class="mb-0 " style="text-align: justify;">FXT4ME is an Automated Forex Trade which allows traders subscribe for a BOT package to enable them trade and earn massively. We also have a working Affiliate System which creates an avenue for an Affiliate to share his/her direct referral link and earn. Our BOT automation trades for 24/7 on market opening days. As an online trader, you can simplify your trade through monitoring and management process. With a secure and compelling mobile-first design, together with a default front-end design, it takes few minutes to setup your account and join the biggest Automated Forex Market.</p>
+                </div>
+                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.3s">
+                    <h5 class="text-white mb-4">Get In Touch</h5>
+                    <p><i class="fa fa-map-marker-alt me-3"></i>Ikorodu, Lagos, Nigeria.</p>
+                    <p><i class="fa fa-phone-alt me-3"></i>+234 903 259 6834</p>
+                    <p><i class="fa fa-envelope me-3"></i>support@fxt4me.com</p>
+                    <div class="d-flex pt-2">
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-twitter"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-facebook-f"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-youtube"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-instagram"></i></a>
+                        <a class="btn btn-outline-light btn-social" href=""><i class="fab fa-linkedin-in"></i></a>
+                    </div>
+                </div>
+                <div class="col-md-6 col-lg-3 wow fadeIn" data-wow-delay="0.5s">
+                    <h5 class="text-white mb-4">Popular Link</h5>
+                    <a class="btn btn-link" href="about">About Us</a>
+                    <a class="btn btn-link" href="contact">Contact Us</a>
+                    <a class="btn btn-link" href="pricing">Plans</a>
+                    <a class="btn btn-link" href="faq">Faqs</a>
+                    <a class="btn btn-link" href="index">Home</a>
+                </div>
+                
+            </div>
+        </div>
+        <div class="container wow fadeIn" data-wow-delay="0.1s">
+            <div class="copyright">
+                <div class="row">
+                    <div class="col-md-6 text-center text-md-start mb-3 mb-md-0">
+                        &copy; <a class="border-bottom" href="">Fxt4me</a>, All Right Reserved.
+                        </div>
+                    <div class="col-md-6 text-center text-md-end">
+                        <div class="footer-menu">
+                            <a href="">Home</a>
+                            <a href="">Cookies</a>
+                            <a href="">Help</a>
+                            <a href="">FAQs</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>`
